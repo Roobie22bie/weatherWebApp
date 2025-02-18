c8205047750b08db9d48f712b8b3adadc1a35bdc
Roobie22bie <robert.j.snars@gmail.com>
2025-02-19 01:50:03 +1100

    These changes don't work yet as of this commit.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 // src/App.js
 import React from 'react';
 import Weather from './Weather.js';
+import ExchangeRates from './ExchangeRates.js';
 import './App.css';
+
+const selectWeatherApp = document.getElementById('displaySpace').innerHTML = <Weather />
+const selectExchangeApp = document.getElementById('displaySpace').innerHTML = <ExchangeRates />
+
 
 function App() {
   return (
@@ -9,10 +14,16 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" class="bi bi-cloud-drizzle" viewBox="0 0 16 16">
-  <path d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973M8.5 2a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4 4 0 0 1 8.5 2"/>
-</svg>
-<br></br>
-        <Weather />
+          <path d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973M8.5 2a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4 4 0 0 1 8.5 2"/>
+        </svg>
+        <br></br>
+        <div id="menu">
+          <button onClick={selectWeatherApp}>Weather App</button>
+          <button onClick={selectExchangeApp}>Check Exchange Rates</button>
+        </div>
+        <div id="displaySpace">
+          <Weather />
+        </div>
       </header>
     </div>
   );
